4da195565c2d1008d76a082549ceeecf09949808
Choi Seungeon <chou610@naver.com>
2019-07-19 09:34:36 +0900

    [update] added new filter

--- src/main/java/com/forestvue/config/WebConfig.java
+++ src/main/java/com/forestvue/config/WebConfig.java
@@ -1,7 +1,9 @@
 package com.forestvue.config;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import javax.servlet.Filter;
 import javax.servlet.ServletRegistration;
 
 public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -19,6 +21,14 @@ public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitiali
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter  characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return new Filter[]{ characterEncodingFilter };
     }
 
     @Override
