cb72b201443ae72d13cc1022e509ca718ef2d3b3
Choi Seungeon <chou610@naver.com>
2019-07-19 09:27:01 +0900

    [update] added component for mapper injection

--- src/main/java/com/forestvue/security/CustomUserDetailsService.java
+++ src/main/java/com/forestvue/security/CustomUserDetailsService.java
@@ -3,14 +3,19 @@ package com.forestvue.security;
 
 import com.forestvue.domain.MemberVO;
 import com.forestvue.mapper.MemberMapper;
+import lombok.AllArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
+@Component
+@AllArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
-    @Setter(onMethod_ = {@Autowired})
+
     private MemberMapper memberMapper;
 
     public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
