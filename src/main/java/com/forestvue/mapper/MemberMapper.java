190a2a1d90933deccde9d96237e7f83a23870286
Choi Seungeon <chou610@naver.com>
2019-07-19 09:41:46 +0900

    [update] mapper features

--- src/main/java/com/forestvue/mapper/MemberMapper.java
+++ src/main/java/com/forestvue/mapper/MemberMapper.java
@@ -3,8 +3,13 @@ package com.forestvue.mapper;
 import com.forestvue.domain.AuthVO;
 import com.forestvue.domain.MemberVO;
 
+import java.util.ArrayList;
+
 public interface MemberMapper {
     public MemberVO read(String username);
+    public ArrayList<MemberVO> readAll();
     public void write(MemberVO vo);
     public void grant(AuthVO vo);
+    public void deleteUserAll();
+    public void deleteUserAuthAll();
 }
